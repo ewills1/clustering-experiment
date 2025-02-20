4a5404e78091df6d2824bd06630017b4e9610945
Edward Wills <ewills1@sheffield.ac.uk>
2025-02-20 11:00:11 +0000

    Update

--- src/run_main.sh
+++ src/run_main.sh
@@ -3,16 +3,18 @@
 #SBATCH --output=clustering_experiment.out
 #SBATCH --error=clustering_experiment.err
 #SBATCH --time=01:00:00  # Set the maximum runtime
-#SBATCH --partition=compute  # Specify the partition/queue
+#SBATCH --partition=generate  # Specify the partition/queue
 #SBATCH --nodes=1  # Number of nodes
 #SBATCH --ntasks=1  # Number of tasks
 #SBATCH --cpus-per-task=4  # Number of CPU cores per task
 #SBATCH --gres=gpu:1  # Request one GPU
 
 # Load necessary modules
-module load python/3.8  # Adjust the Python version as needed
+module load Anaconda3/2024.02-1  # Adjust the Python version as needed
 module load cuda/11.2  # Adjust the CUDA version as needed
 module load dissertation-experiment1
 
+source activate mynumpy # Activate the virtual environment
+
 # Run the Python script
-python dissertation-experiment1/src/main.py
+srun python ~/dissertation-experiment1/src/main.py
